3d894c3a70eccb520d7f0adc82084beb85f56333
ckt22 <cktangad@connect.ust.hk>
2020-09-24 22:14:01 +0800

    override the header

--- screens/diet/EditDietScreen.js
+++ screens/diet/EditDietScreen.js
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import { Button, Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Overriding the header
+DetailsScreen['navigationOptions'] = screenProps => ({
+    title: "",
+    headerLeft: () => <Ionicons name={'ios-arrow-back'} size={30} color={'dodgerblue'} style={{paddingLeft: 10}} onPress={() => screenProps.navigation.goBack()}/>
+})
 
 // screen for demo purpose
-export default function DetailsScreen() {
+export default function DetailsScreen({ navigation }) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Details!</Text>
