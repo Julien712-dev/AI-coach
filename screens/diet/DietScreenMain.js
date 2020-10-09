38f8ad374c25c5cf511253b6e16749d347958575
ckt22 <cktangad@connect.ust.hk>
2020-10-10 00:12:37 +0800

    ui view demo

--- screens/diet/DietScreenMain.js
+++ screens/diet/DietScreenMain.js
@@ -1,14 +1,53 @@
 import * as React from 'react';
-import { View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import moment from 'moment';
+import { View, StyleSheet, ScrollView, SafeAreaView } from 'react-native';
+import { Button, Text, Card, Title, Paragraph } from 'react-native-paper';
+
 
 export default function DietScreenMain({ navigation }) {
+
+	const [time, setTime] = React.useState();
+	React.useEffect(() => {
+		let currentTime = new Date();
+		setTime(currentTime);
+	}, [])
+
 	return (
-		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-			<Text>Diet Screen Main</Text>
-			<Button mode='contained' onPress={() => navigation.navigate('Edit Diet')}>
-				Edit Diet
-			</Button>
-		</View>
+		// <SafeAreaView>
+			<ScrollView>
+				<Title>{moment(time).format('LLLL')}</Title>
+				<Button mode='contained' onPress={() => navigation.navigate('Edit Diet')}>
+					Edit Diet
+				</Button>
+				<Title>Explore</Title>
+				<Card>
+					<Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+					<Card.Content>
+						<Title>Card title</Title>
+						<Paragraph>Card content</Paragraph>
+					</Card.Content>
+					<Card.Actions>
+						<Button>OK</Button>
+					</Card.Actions>
+				</Card>
+				<Card>
+					<Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+					<Card.Content>
+						<Title>Card title 2</Title>
+						<Paragraph>Card content 2</Paragraph>
+					</Card.Content>
+					<Card.Actions>
+						<Button>OK</Button>
+					</Card.Actions>
+				</Card>
+			</ScrollView>
+		// </SafeAreaView>
 	);
 }
+
+const styles = StyleSheet.create({
+	card: {
+		width: 0.4,
+		height: 50
+	}
+});
