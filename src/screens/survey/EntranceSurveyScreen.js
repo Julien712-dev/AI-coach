b3ce4c6536059ed95c52d4a1b5f126b2783920ec
ckt22 <cktangad@connect.ust.hk>
2020-12-04 18:19:01 +0800

    entrance screen step 2 completed

--- src/screens/survey/EntranceSurveyScreen.js
+++ src/screens/survey/EntranceSurveyScreen.js
@@ -1,20 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useReducer } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Firebase from 'firebase';
 import config from '../../config';
 import moment from 'moment';
-import { View, StyleSheet, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native';
-import { Button, Text, Title, Card, Paragraph, TextInput, Chip } from 'react-native-paper';
+import { View, StyleSheet, ScrollView, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { Button, Text, Title, Card, Paragraph, TextInput, Chip, RadioButton, Snackbar } from 'react-native-paper';
 import DropDown from 'react-native-paper-dropdown';
 import Carousel from 'react-native-snap-carousel';
 import LoadingScreen from "../LoadingScreen";
-import { saveProfile, updateProfile } from "../../actions/profile.actions";
 import { updateTempStorage, saveProfileToFirebase } from "../../store/profileSlice.js";
-
-import StepIndicator from 'react-native-step-indicator';
 import Swiper from 'react-native-swiper';
 
-function EntranceSurveyStepOneScreen({ navigation }) {
+function EntranceSurveyStepOneScreen({ navigation, swiperRef }) {
 
     const [firstName, setFirstName] = useState(null);
     const [lastName, setLastName] = useState(null);
@@ -47,14 +44,19 @@ function EntranceSurveyStepOneScreen({ navigation }) {
     
     return (
         <View style={{ flex: 1 }}>
-            <View style={{ flexDirection: "row", width: 365, marginBottom: 8 }}>
+            <View style={{ height: 400}}>
+            <View style={{ marginHorizontal: 10 }}>
+                <Title>Step 1: About You</Title>
+            </View>
+            <View style={{ flexDirection: "row", width: 365, marginBottom: 20 }}>
                 <TextInput style={{width: 165, marginHorizontal: 10}} label='First Name' mode='outlined' value={firstName} onChangeText={text => setFirstName(text)} autoCorrect={false}></TextInput>
                 <TextInput style={{width: 165, marginHorizontal: 10}} label='Last Name' mode='outlined' value={lastName} onChangeText={text => setLastName(text)} autoCorrect={false}></TextInput>
             </View>
-            <View style={{ flexDirection: 'row', width: 365, marginBottom: 8 }}>
+            <View style={{ flexDirection: 'row', width: 365, marginBottom: 20 }}>
                 <TextInput style={{ width: 220, marginHorizontal: 10 }} label='Height' mode='outlined' value={height} keyboardType={"number-pad"} onChangeText={text => setHeight(text)} autoCorrect={false}></TextInput>
                 <View style={{ width: 120}}>
                     <DropDown
+                        label={'Height Unit'}
                         mode={'outlined'}
                         value={heightUnit}
                         setValue={setHeightUnit}
@@ -68,7 +70,7 @@ function EntranceSurveyStepOneScreen({ navigation }) {
                     />
                 </View>
             </View>
-            <View style={{ flexDirection: "row", width: 365, marginBottom: 8 }}>
+            <View style={{ flexDirection: "row", width: 365, marginBottom: 20 }}>
                 <TextInput style={{ width: 220, marginHorizontal: 10 }} label='Weight' mode='outlined' value={weight} keyboardType={"number-pad"} onChangeText={text => setWeight(text)} autoCorrect={false}></TextInput>
                 <View style={{ width: 120 }}>
                     <DropDown
@@ -86,7 +88,7 @@ function EntranceSurveyStepOneScreen({ navigation }) {
                     />
                 </View>
             </View>
-            <View style={{ flexDirection: "row", width: 365, marginBottom: 8 }}>
+            <View style={{ flexDirection: "row", width: 365, marginBottom: 20 }}>
                 <TextInput style={{ width: 220, marginHorizontal: 10 }} label='Age' mode='outlined' value={age} keyboardType={"number-pad"} onChangeText={text => setAge(text)} autoCorrect={false}></TextInput>
                 <View style={{ width: 120 }}>
                     <DropDown
@@ -104,39 +106,150 @@ function EntranceSurveyStepOneScreen({ navigation }) {
                     />
                 </View>
             </View>
-            <Button style={styles.btnStyle} mode='contained' disabled={!isValid} onPress={() => {
-                let setObj = {
-                    firstName: !!firstName ? firstName: undefined,
-                    lastName: !!lastName ? lastName: undefined, 
-                    height: !!height ? height: undefined, 
-                    weight: !!weight ? weight: undefined, 
-                    heightUnit: !!heightUnit ? heightUnit: undefined, 
-                    weightUnit: !!weightUnit ? weightUnit: undefined,
-                    sex: !!sex? sex: undefined
-                }
-                dispatch(updateTempStorage(setObj));
-                const userFireBaseProfileRef = Firebase.database().ref(`/users/${user.uid}/profile`);
-                dispatch(saveProfileToFirebase(userFireBaseProfileRef));
-                navigation.goBack();
-                }}>create profile</Button>
+            </View>
+            <View style={{ marginHorizontal: 10, flexDirection: "row", alignContent: "center", justifyContent: "center" }}>
+                <Button style={{width: 130}} mode='contained' disabled={!isValid} onPress={() => {
+                    let setObj = {
+                        firstName: !!firstName ? firstName: undefined,
+                        lastName: !!lastName ? lastName: undefined, 
+                        height: !!height ? height: undefined, 
+                        weight: !!weight ? weight: undefined, 
+                        heightUnit: !!heightUnit ? heightUnit: undefined, 
+                        weightUnit: !!weightUnit ? weightUnit: undefined,
+                        sex: !!sex? sex: undefined
+                    }
+                    dispatch(updateTempStorage(setObj));
+                    // const userFireBaseProfileRef = Firebase.database().ref(`/users/${user.uid}/profile`);
+                    // dispatch(saveProfileToFirebase(userFireBaseProfileRef));
+                    swiperRef.current.scrollBy(1)
+                    }}>NEXT</Button>
+            </View>
         </View>
     )
 }
 
-function EntranceSurveyStepTwoScreen({ navigation }) {
-    return (
-        <View>
-            <Text>Step Two</Text>
-        </View>
-    )
+function ChipOfSingleChoice() {
+    return (<Chip></Chip>)
 }
 
-function EntranceSurveyStepThreeScreen({ navigation }) {
 
+function EntranceSurveyStepTwoScreen({ navigation, swiperRef }) {
+
+    const [value, setValue] = useState();
+    const [isSelected, setIsSelected] = useState(false);
+    const [exerciseHabit, setExerciseHabit] = useState(null);
+    const [isValid, setIsValid] = useState(false);
+
+    const dispatch = useDispatch();
     
+    const surveyFields = [exerciseHabit]
+    const validateSurvey = () => {
+        for (var surveyField of surveyFields) {
+            if (!surveyField) return;
+        }
+        setIsValid(true);
+    }
+
+    useEffect(() => {
+        validateSurvey();
+    }, [exerciseHabit])
+
+    const renderSurveyOptions = (props) => {
+        let {
+            setValueFunction,
+            displayText,
+            currentValue,
+            value
+        } = props;
+
+        return (
+            <TouchableOpacity style={{marginVertical: 5, padding: 15, width: 325, borderRadius: 15, borderWidth: (currentValue == value) ? 2 : 0.85, borderColor: (currentValue == value)? 'green': 'black' }} onPress={() => setValueFunction(value)}>
+                <Text>{displayText}</Text>
+            </TouchableOpacity>
+        )
+    }
+
+
     return (
-        <View>
-            <Text>Step Three</Text>
+        <View style={{ flex: 1 }}>
+            <View style={{ height: 400}}>
+                <View style={{ marginHorizontal: 10 }}>
+                    <Title>Step 2: Exercise Habits</Title>
+                </View>
+                <View style={{ marginHorizontal: 10 }}>
+                    <Text>How often do you exercise weekly?</Text> 
+                </View>
+                <View style={{ marginHorizontal: 10 }}>
+                    {config.constants.exerciseHabitOptions.map(option => (renderSurveyOptions({setValueFunction: setExerciseHabit, displayText: option.label, value: option.value, currentValue: exerciseHabit})))}
+                </View>
+            </View>
+
+            <View style={{ marginHorizontal: 10, flexDirection: "row", alignContent: "center", justifyContent: "center" }}>
+                <Button 
+                    style={{marginHorizontal: 5, width: 130}} 
+                    mode="contained"
+                    onPress={() => swiperRef.current.scrollBy(-1)}
+                
+                >PREVIOUS</Button>
+                <Button 
+                    disabled={!isValid} 
+                    style={{ marginHorizontal: 5, width: 130 }} 
+                    mode="contained"
+                    onPress={() => {
+                        let setObj = {
+                            exerciseHabit: !!exerciseHabit ? exerciseHabit : undefined
+                        }
+                        dispatch(updateTempStorage(setObj));
+                        swiperRef.current.scrollBy(1);
+                    }}
+                >NEXT</Button>
+            </View>
+        </View>
+    )
+}
+
+function EntranceSurveyStepThreeScreen({ navigation, swiperRef }) {
+
+    const [isValid, setIsValid] = useState(false);
+    const [alert, setAlert] = useState('');
+    const [visible, setVisible] = useState(false);
+    const [foodAllergies, setFoodAllergies] = useState([]);
+    const dispatch = useDispatch();
+
+	let user = useSelector(state => state.main.auth.user);
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={{ height: 400}}>
+                <View style={{ marginHorizontal: 10 }}>
+                    <Title>Step 3: Diet Preferences</Title>
+                </View>
+            </View>
+            <View style={{ marginHorizontal: 10, flexDirection: "row", alignContent: "center", justifyContent: "center" }}>
+                <Button 
+                    style={{marginHorizontal: 5, width: 130}} 
+                    mode="contained"
+                    onPress={() => swiperRef.current.scrollBy(-1)}
+                
+                >PREVIOUS</Button>
+                <Button
+                    style={{ marginHorizontal: 5, width: 130 }} 
+                    mode="contained"
+                    onPress={() => {
+                        // let setObj = {
+                        //     exerciseHabit: !!exerciseHabit ? exerciseHabit : undefined
+                        // }
+                        // dispatch(updateTempStorage(setObj));
+                        setAlert('Your profile has been updated')
+                        setVisible(true);
+                        const userFireBaseProfileRef = Firebase.database().ref(`/users/${user.uid}/profile`);
+                        dispatch(saveProfileToFirebase(userFireBaseProfileRef));
+                        setTimeout(() => {
+                            navigation.goBack();
+                        }, 1000)
+                    }}
+                >SAVE</Button>
+            </View>
+            <Snackbar visible={visible} onDismiss={() => setVisible(false)} duration={3000}>{alert}</Snackbar>
         </View>
     )
 }
@@ -144,8 +257,6 @@ function EntranceSurveyStepThreeScreen({ navigation }) {
 // MAIN
 export default function EntranceSurveyScreen({ navigation }) {
 
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
     const [profile, setProfile] = useState(null);
     const [isFetched, setIsFetched] = useState(false);
     const [currentPage, setCurrentPage] = useState(0);
@@ -157,9 +268,9 @@ export default function EntranceSurveyScreen({ navigation }) {
     let tempProfile = useSelector(state => state.main.profile);
 
     const PAGES = [
-        <EntranceSurveyStepOneScreen navigation={navigation} />, 
-        <EntranceSurveyStepTwoScreen navigation={navigation} />, 
-        <EntranceSurveyStepThreeScreen navigation={navigation} />
+        <EntranceSurveyStepOneScreen navigation={navigation} swiperRef={swiperRef} />, 
+        <EntranceSurveyStepTwoScreen navigation={navigation} swiperRef={swiperRef} />, 
+        <EntranceSurveyStepThreeScreen navigation={navigation} swiperRef={swiperRef} />
     ];
 
 
@@ -187,43 +298,21 @@ export default function EntranceSurveyScreen({ navigation }) {
 		return unsubscribe;
 	  }, [navigation]);
 
-    const createProfile = () => {
-        const profileRef = Firebase.database().ref(`users/${user.uid}`);
-        profileRef.set({
-            ...profile, 
-            profile: {
-            firstName: firstName, 
-            lastName: lastName
-        }}).then(() => {
-            navigation.goBack();
-        });
-    };
-
     if (!user || !isFetched) {
         return (<LoadingScreen />)
     } else {
     return (
         <ScrollView>
             <View style={{flex: 1}}>
-            {/* <Text>{user.uid}</Text> */}
-            <Text>{JSON.stringify(tempProfile)}</Text>
+            {/* <Text>{JSON.stringify(tempProfile)}</Text> */}
             <Button icon="close" style={{ position: 'absolute', top:20, right:0 }} onPress={() => navigation.goBack()}>Skip</Button>
                 <View style={{ padding: 20, marginTop: 50, justifyContent: 'center', flex: 1 }}>
                     <Text>Set up your profile for a better user experience! </Text>
                 </View>
             </View>
-            {/* <View style={{ flex: 1, flexDirection: "row" }}>
-                <TextInput style={styles.inputStyle} label='First Name' mode='outlined' value={firstName} onChangeText={text => setFirstName(text)} autoCorrect={false}></TextInput>
-                <TextInput style={styles.inputStyle} label='Last Name' mode='outlined' value={lastName} onChangeText={text => setLastName(text)} autoCorrect={false}></TextInput>
-            </View> */}
-            {/* <Button style={styles.btnStyle} mode='contained' onPress={() => {dispatch(updateProfile({ newData: { firstName: firstName, lastName: lastName }}))}}>update dispatch</Button>
-            <Button style={styles.btnStyle} mode='contained' onPress={() => createProfile()}>create profile</Button>
-            <Button style={styles.btnStyle} mode='contained' onPress={() => {dispatch(saveProfile())}}>create profile</Button>
-            <Button style={styles.btnStyle} mode='contained' disabled={currentPage >= PAGES.length-1} onPress={() => swiperRef.current.scrollBy(1)}>Next Page</Button>
-            <Button style={styles.btnStyle} mode='contained' disabled={currentPage == 0} onPress={() => swiperRef.current.scrollBy(-1)}>Go Back</Button> */}
             <Swiper
                 ref={swiperRef}
-                style={{ height: 460 }}
+                style={{ height: 500 }}
                 loop={false}
                 index={currentPage}
                 autoplay={false}
@@ -235,9 +324,6 @@ export default function EntranceSurveyScreen({ navigation }) {
             >
                 {PAGES.map((page) => (page))}
             </Swiper>
-
-            <Button style={styles.btnStyle} mode='contained' disabled={currentPage >= PAGES.length-1} onPress={() => swiperRef.current.scrollBy(1)}>Next Page</Button>
-            <Button style={styles.btnStyle} mode='contained' disabled={currentPage == 0} onPress={() => swiperRef.current.scrollBy(-1)}>Go Back</Button>
         </ScrollView>
     )
     }
