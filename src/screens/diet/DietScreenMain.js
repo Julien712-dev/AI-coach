807cf7d2b15bfc838b186fbd6f9f97d3a4d33574
ckt22 <cktangad@connect.ust.hk>
2021-01-20 12:40:44 +0800

    diet screen code cleanup

--- src/screens/diet/DietScreenMain.js
+++ src/screens/diet/DietScreenMain.js
@@ -85,46 +85,22 @@ export default function DietScreenMain({ navigation }) {
 			console.log(nutritionValues);
 
 			await searchByName({
-				meal: meal.meal,
-				minCalories: nutritionValues.dailyRecommendedCalories * 0.7 * meal.weight,
+				type: meal.meal,
+				// minCalories: nutritionValues.dailyRecommendedCalories * 0.7 * meal.weight,
 				maxCalories: nutritionValues.dailyRecommendedCalories * meal.weight,
 				maxCarbs: nutritionValues.maximumDailyCarbsInGrams * meal.weight,
 				maxProtein: nutritionValues.maximumDailyProteinInGrams * meal.weight,
 				maxFat: nutritionValues.maximumDailyFatsInGrams * meal.weight
 			});
 
-			// console.log("Starting Location Update");
-			// Location.startLocationUpdatesAsync('TASK_FETCH_LOCATION', {
-			//   accuracy: Location.Accuracy.Highest,
-			//   distanceInterval: 1, // minimum change (in meters) betweens updates
-			//   deferredUpdatesInterval: 10000, // minimum interval (in milliseconds) between updates
-			//   // foregroundService is how you get the task to be updated as often as would be if the app was open
-			//   foregroundService: {
-			// 	notificationTitle: 'Using your location',
-			// 	notificationBody: 'To turn off, go back to the app and switch something off.'
-			//   },
-			// });
-	
-			// subscriber = await Location.watchPositionAsync(
-			//   {
-			// 	accuracy: Accuracy.BestForNavigation,
-			// 	timeInterval: 1000,
-			// 	distanceInterval: 10,
-			//   },
-			//   () => {
-			// 	startWatching(true);
-			//   }
-			// );
 		  })();
 	}, []);
 
-	// When the user first login, force the user to complete his profile.
+	// Fires when users click into the screen
 	useEffect(() => {
 		const unsubscribe = navigation.addListener('focus', () => {
-			console.log('unsub func triggered');
 			// The screen is focused
 			// Call any action
-			// console.log(profileRedux);
 		});
 	
 		// Return the function to unsubscribe from the event so it gets removed on unmount
@@ -231,25 +207,3 @@ export default function DietScreenMain({ navigation }) {
 		</View>
 	);
 }
-
-// Background Tasks for defnining 
-// console.log("Defining task");
-// TaskManager.defineTask(TASK_FETCH_LOCATION, async ({ data: { locations }, error }) => {
-//   if (error) {
-//     console.error(error);
-//     return;
-//   }
-//   const [location] = locations;
-
-//   let jobId = await AsyncStorage.getItem('jobId');
-
-//   try {
-//     const url = `/deliver-job/location/report`;
-//     await rbFleetApi.post(url, { 
-//       location, 
-//       jobId 
-//     }); // you should use post instead of get to persist data on the backend
-//   } catch (err) {
-//     console.error(err);
-//   }
-// });
