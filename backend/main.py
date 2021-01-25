fe4dd889b0eb61f8fcc8c7b49e061703efaa62fd
Clong <isaacchanszelong@gmail.com>
2021-01-25 15:58:00 +0800

    Add tensorflow into backend

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import tensorflow as tf
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+model = tf.keras.models.load_model('model')
+class_labels = ['push-up-arms-not-bent-enough', 'push-up-normal', 'push-up-waist-too-low']
+
+@app.route('/', methods=['GET'])
+def root():
     return 'Exercise classifier running'
 
+@app.route('/classify', methods=['POST'])
+def classify():
+    data = request.json
+    timeseries = tf.convert_to_tensor(data['timeseries'])
+    probabilities = model.predict(timeseries)
+    index_result = list(tf.math.argmax(probabilities, axis=1).numpy())
+    label_result = [class_labels[index] for index in index_result]
+    return jsonify(label_result)
 
 if __name__ == '__main__':
     # This is used when running locally only.
