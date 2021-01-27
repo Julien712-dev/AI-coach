da9f7172b06e3607964603e391f1f4b2f7cc3b4f
Clong <isaacchanszelong@gmail.com>
2021-01-27 11:53:25 +0800

    Updating test

--- backend/test/test.bat
+++ backend/test/test.bat
@@ -1,1 +1,1 @@
-curl -i -X POST -H "Content-Type: application/json" -d @input.json http://127.0.0.1:8080/classify
+curl -i -X POST -H "Content-Type: application/json" -d @input.json http://localhost:8080/classify
